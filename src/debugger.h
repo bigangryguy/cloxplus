7453517b416f4ba29f2595205e5992d7cfa7e19e
David Wilcox <davidtwilcox@gmail.com>
2020-08-14 06:39:21 -0500

    Restructured and added tests
    Restructured and added tests

--- src/debugger.h
+++ src/debugger.h
@@ -9,10 +9,10 @@ class Debugger {
  public:
   static std::string DisassembleChunk(const Chunk& chunk,
                                        const std::string& name);
+private:
   static size_t DisassembleInstruction(const Chunk& chunk,
                                        size_t offset,
                                        std::string& output);
-private:
   static size_t SimpleInstruction(const std::string& name,
                                   size_t offset,
                                   std::string& output);
