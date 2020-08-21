74735cba8887412a7a9d254c8d6a30c6bb214b4a
David Wilcox <davidtwilcox@gmail.com>
2020-08-21 14:35:30 -0500

    OP_CONSTANT_LONG to support more than 256 constants per chunk
    OP_CONSTANT_LONG to support more than 256 constants per chunk

--- src/opcode.h
+++ src/opcode.h
@@ -4,7 +4,11 @@
 
 namespace cloxplus {
 enum OpCode : uint8_t {
+  // Operand: 1-byte offset to index of constant
   OP_CONSTANT,
+  // Operand: 3-byte offset to index of constant
+  OP_CONSTANT_LONG,
+  // Operand: None
   OP_RETURN
 };
 }
